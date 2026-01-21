05428c49d659e81dce67895db000a94e12fdebc0
Clésio Adriano <103958657+clesioadriano14@users.noreply.github.com>
2026-01-21 13:14:33 -0300

    Intro_Arquivos

---
+++ Introdução_Arquivos/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+    /*
+    ofstream arq;
+    arq.open("arquivo.txt", ios::app);
+    if (!arq.is_open())
+    {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
+    arq << "Exemplo de texto de arquivo" << endl;
+    arq.put('P');
+    //arq.flush();
+    arq.close();
+    */
+   /*
+    ifstream arq;
+    arq.open("arquivo.txt");
+    if (!arq.is_open())
+    {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
+    char linha[256];
+    char letra;
+
+    while (!arq.eof())
+    {
+    arq.getline(linha, 256);
+    arq.get(letra);
+    arq.ignore();
+
+    cout << "Linha: " << linha << endl;
+    cout << "Caractere: " << letra << endl;
+    }
+    */
+    int tmemoria, fitas, i;
+    char c;
+    vector<char>mem;
+    fstream arq("dados.in", ios::in);
+    arq >> tmemoria >> fitas;
+    arq.ignore();
+
+    cout << "Fitas: " << fitas << endl;
+    cout << "T. Memoria: " << tmemoria << endl;
+
+    while (!arq.eof())
+    {
+        i = 0;
+        while (i < tmemoria  && !arq.eof())
+        {
+            arq.get(c);
+            mem.push_back(c);
+            i++;
+        }
+        sort(mem.begin(), mem.end());
+        cout << string (mem.begin(), mem.end()) << endl;
+        mem.clear();
+    }
+
+    arq.close();
+
+    return 0;
+} 
